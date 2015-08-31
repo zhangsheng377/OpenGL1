296039e8f783423d7313341368e97d9d53d931ac
张胜东 <435878393@qq.com>
2015-08-31 20:19:16 +0800

    初始

---
+++ hw1-windows/Transform.cpp
@@ -0,0 +1,44 @@
+// Transform.cpp: implementation of the Transform class.
+
+
+#include "Transform.h"
+#include <iostream>
+
+//Please implement the following functions:
+
+// Helper rotation function.  
+mat3 Transform::rotate(const float degrees, const vec3& axis) {
+  // YOUR CODE FOR HW1 HERE
+
+  // You will change this return call
+  return mat3();
+}
+
+// Transforms the camera left around the "crystal ball" interface
+void Transform::left(float degrees, vec3& eye, vec3& up) {
+  // YOUR CODE FOR HW1 HERE
+	printf("Coordinates: %.2f, %.2f, %.2f; distance: %.2f\n", eye.x, eye.y, eye.z, sqrt(pow(eye.x, 2) + pow(eye.y, 2) + pow(eye.z, 2)));
+}
+
+// Transforms the camera up around the "crystal ball" interface
+void Transform::up(float degrees, vec3& eye, vec3& up) {
+  // YOUR CODE FOR HW1 HERE 
+}
+
+// Your implementation of the glm::lookAt matrix
+mat4 Transform::lookAt(vec3 eye, vec3 up) {
+  // YOUR CODE FOR HW1 HERE
+
+  // You will change this return call
+  return mat4();
+}
+
+Transform::Transform()
+{
+
+}
+
+Transform::~Transform()
+{
+
+}
