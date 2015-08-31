288b054eca024b51268643bca6d9b9a57da4cb63
张胜东 <435878393@qq.com>
2015-08-31 23:20:47 +0800

    完成生成旋转矩阵和左右旋转物体

--- hw1-windows/Transform.cpp
+++ hw1-windows/Transform.cpp
@@ -11,13 +11,19 @@ mat3 Transform::rotate(const float degrees, const vec3& axis) {
   // YOUR CODE FOR HW1 HERE
 
   // You will change this return call
-  return mat3();
+	mat3 M, M1, M2;
+	M1 = mat3(axis.x*axis.x, axis.x*axis.y, axis.x*axis.z, axis.x*axis.y, axis.y*axis.y, axis.y*axis.z, axis.x*axis.z, axis.y*axis.z, axis.z*axis.z);
+	M2 = mat3(0, axis.z, -axis.y, -axis.z, 0, axis.x, axis.y, -axis.x, 0);
+	M = cos(degrees / 180 * pi)*mat3(1.0) + (1 - cos(degrees / 180 * pi))*M1 + sin(degrees / 180 * pi)*M2;
+  return M;
 }
 
 // Transforms the camera left around the "crystal ball" interface
 void Transform::left(float degrees, vec3& eye, vec3& up) {
   // YOUR CODE FOR HW1 HERE
-	printf("Coordinates: %.2f, %.2f, %.2f; distance: %.2f\n", eye.x, eye.y, eye.z, sqrt(pow(eye.x, 2) + pow(eye.y, 2) + pow(eye.z, 2)));
+	vec3 y = vec3(0, 1, 0);
+	eye = rotate(degrees, y)*eye;
+	up = rotate(degrees, y)*up;
 }
 
 // Transforms the camera up around the "crystal ball" interface
